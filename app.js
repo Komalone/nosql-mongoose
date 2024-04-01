17613a655aa872f50bea759c9c69b79acae3f783
KomalOne <komalmandal1998@gmail.com>
2024-04-02 02:38:06 +0530

    data added, edit, delete

---
+++ app.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const cors= require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose= require('mongoose')
+
+const errorController = require('./controllers/error');
+// const User= require('./models/user')
+
+const app = express();
+app.use(cors());
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use((req, res, next) => {
+//   User.findByPk('6609b807f46cb8c63f6062f5')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+.connect('mongodb+srv://komal123:komal123@atlascluster.qquzra1.mongodb.net/shop?retryWrites=true&w=majority&appName=AtlasCluster')
+.then(result=>{
+  app.listen(3000);
+}).catch(err =>{
+  console.log(err);
+})
---
+++ models/product.js
@@ -0,0 +1,102 @@
+const mongoose= require('mongoose');
+
+const Schema= mongoose.Schema;
+
+const productSchema= new Schema({
+  title:{
+    type: String,
+    required: true
+  },
+  price:{
+    type: Number,
+    required: true
+  },
+  imageUrl:{
+    type: String,
+    required: true
+  },
+  description:{
+    type: String,
+    required: true
+  }
+})
+
+module.exports= mongoose.model('Product', productSchema)
+
+// const mongodb= require('mongodb')
+// const getDb= require('../util/database').getDb
+
+// class Product{
+//   constructor(title, price, imageUrl, description, id, userId){
+//     this.title= title;
+//     this.price= price;
+//     this.imageUrl= imageUrl;
+//     this.description= description;
+//     this._id= id ? new mongodb.ObjectId(id) : null;
+//     this.userId = userId;
+//   }
+
+//   save(){
+//     const db= getDb();
+//     let dbOp;
+//     if(this._id){
+//       dbOp= db.collection('products')
+//       .updateOne({_id:this._id}, {$set: this})
+//     }else{
+//       dbOp= db.collection('products')
+//       .insertOne(this)
+//     }
+//     return dbOp
+//     .then(result =>{
+//       console.log("db result",result)
+//     })
+//     .catch(err=> {
+//       console.log(err);
+//     })
+//   }
+
+//   static fetchAll(){
+//     const db= getDb();
+//     return db.collection('products')
+//     .find()
+//     .toArray()
+//     .then(prod =>{
+//       console.log(prod);
+//       return prod;
+//     })
+//     .catch(err =>{
+//       console.log(err)
+//     })
+//   }
+
+//   static findById(prodId){
+//     const db= getDb();
+//     return db.collection('products')
+//     .find({_id : new mongodb.ObjectId(prodId)})
+//     .next()
+//     .then(prod =>{
+//       console.log(prod);
+//       return prod;
+//     })
+//     .catch(err =>{
+//       console.log(err)
+//     })
+//   }
+
+//   static deleteById(prodId){
+//     const db= getDb();
+//     return db.collection('products')
+//     .deleteOne({_id: new mongodb.ObjectId(prodId)})
+//     .then(result =>{
+//       console.log('deleted');
+//       return prod;
+//     })
+//     .catch(err =>{
+//       console.log(err)
+//     })
+//   }
+// }
+
+
+
+// module.exports = Product;
