cdeb67c26ea1f33641a590fde828a3a8a9552009
KomalOne <komalmandal1998@gmail.com>
2024-04-03 02:59:14 +0530

    user model used

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose= require('mongoose')
 
 const errorController = require('./controllers/error');
-// const User= require('./models/user')
+const User= require('./models/user')
 
 const app = express();
 app.use(cors());
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findByPk('6609b807f46cb8c63f6062f5')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findByPk('6609b807f46cb8c63f6062f5')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -35,6 +35,19 @@ app.use(errorController.get404);
 mongoose
 .connect('mongodb+srv://komal123:komal123@atlascluster.qquzra1.mongodb.net/shop?retryWrites=true&w=majority&appName=AtlasCluster')
 .then(result=>{
+  User.findOne().then(user =>{
+    if(!user){
+      const user= new User({
+    name: "komal",
+    email: "komal@gmail.com",
+    cart: {
+      items: []
+    }
+  });
+  user.save();
+    }
+  });
+  
   app.listen(3000);
 }).catch(err =>{
   console.log(err);
